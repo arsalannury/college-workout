f2195a9187eb6c0724cb107fa96ee18376b9ea88
arsalannury <arsalannury@gmail.com>
2022-06-20 11:59:20 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WorkByDirectoryAndFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           File.Create("N:\\TestFolder\\SubFolder\\text.txt");
+           Console.WriteLine("created");
+           Console.ReadKey();
+        }
+    }
+}
+
+
